ddf0c9dc5300b265d80dcdba500403c04818e8d5
Quintisimo <quintuscardozo13@gmail.com>
2017-06-14 23:54:01 +1000

    Added gulp task to auto-reload broswer

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var harp = require('harp');
+
+gulp.task('serve', function () {
+  harp.server('.', {
+    port: 9000
+  }, function() {
+    browserSync.init({
+      proxy: "localhost:9000"
+    });
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch("_includes/jade/*.jade", browserSync.reload);
+  gulp.watch("_includes/sass/*.sass", browserSync.reload);
+});
+
+gulp.task('default', ['serve', 'watch']);
