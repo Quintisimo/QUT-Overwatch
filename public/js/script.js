8ad654cfa5e759b78460b193bca815938ba03fa2
Quintisimo <quintuscardozo13@gmail.com>
2017-06-28 14:48:06 +1000

    Fixed website for landscape view on mobile

--- public/js/script.js
+++ public/js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - 120
       }, 800, function(){
         window.location.hash = hash;
       });
