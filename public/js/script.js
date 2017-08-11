0564da8eef26913c1224972cc8bb16e64912aa86
Quintisimo <quintuscardozo13@gmail.com>
2017-08-11 23:07:55 +1000

    Added code comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// Facebook widget
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -6,22 +7,29 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+// jQuery
 (function($) {
-   function dismissNav() {
-     $('.mobile-nav').removeClass('is-visible');
-   }
 
   $(document).ready(function() {
+
+    // Dismisses mobile navigation
+    function dismissNav() {
+      $('.mobile-nav').removeClass('is-visible');
+    }
+
+    // Filps persons face on click
     $('.person').click(function() {
       $(this).toggleClass('flipped');
     });
 
+    // Displays players rank when rank is clicked
     $('.player-id').click(function() {
       var rank = $(this).children().text();
       rank = rank.replace("#", "-");
       window.open("https://masteroverwatch.com/profile/pc/us/" + rank);
     });
 
+    // Smooth scroll
     $('a').on('click', function(event) {
       var size = $('html').css('font-size').replace('px', ''),
       height = 8*size;
@@ -37,11 +45,13 @@
       }
     });
 
+    // Animates mobile navigation on click
     $('#mobile-nav-toggle').click(function(e) {
       $('.mobile-nav').addClass('is-visible');
       e.stopPropagation();
     });
 
+    // Dismisses mobile navgation
     $('#mobile-nav-dismiss').click(dismissNav);
     $(document).click(dismissNav);
   });
